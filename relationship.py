0de13b6c93978f2f7c5d81b0c2eb641cc9b44913
Rieznichenko <RieznichenkoIhor@gmail.com>
2024-05-19 13:50:25 +0300

    fix: introduce link collection concept

--- relationship.py
+++ relationship.py
@@ -1,122 +1,93 @@
+import logging
 import pandas as pd
-from pymongo import MongoClient, UpdateOne
+from pymongo import MongoClient
 from config import parse_cli
-from collections import deque
 import json
-import random
 import uuid
-import copy
 from bson.objectid import ObjectId
-import sys
+import copy
 
-sys.setrecursionlimit(200000)
 class Relationship:
     def __init__(self, args):
-        self.chunk_size = 1
+        self.chunk_size = 10000
         self.count = 0
-        # MongoDB configuration
         self.mongo_uri = f"mongodb://{args.mongo_host}:{args.mongo_port}"
         self.mongo_db = args.mongo_db
         self.mongo_dummy_collection = args.mongo_dummy_collection
         self.mongo_relation_collection = args.mongo_relation_collection
-
-        client = MongoClient(self.mongo_uri)
-        db = client[self.mongo_db]
-        collection = db[self.mongo_dummy_collection]
-        self.data = self.convert_objectid(list(collection.find()))
-
-        self.link = {}
+        self.client = MongoClient(self.mongo_uri)
+        self.db = self.client[self.mongo_db]
+        self.collection = self.db[self.mongo_dummy_collection]
+        self.ip_address_collection = self.db["ip_address_link"]
+        self.domain_collection = self.db["domain_link"]
+        self.sha256_collection = self.db["sha256_link"]
+        self.ip_address_collection.create_index("value")
+        self.domain_collection.create_index("value")
+        self.sha256_collection.create_index("value")
+        self.data = self.load_data()
+        self.link = {i: {} for i in range(3)}
         self.vis = {}
         self.mp = {}
-        for i in range(0, 3):
-            self.link[i] = {}
+        self.init_data()
 
-        for d in self.data:
-            self.mp[d['id']] = d
-            self.link[0][d['id']] = []
-            self.link[1][d['id']] = []
-            self.link[2][d['id']] = []
-
-            if d["ip_address"] is not None:
-                self.link[0][d['id']] = self.convert_objectid(list(collection.find({"ip_address": d["ip_address"]})))
-            if d["domain"] is not None:
-                self.link[1][d['id']] = self.convert_objectid(list(collection.find({"domain_name": d["domain"]})))
-            if d["sha256"] is not None:
-                self.link[2][d['id']] = self.convert_objectid(list(collection.find({"file_hash": d["sha256"]})))
-
-            self.vis[d['id']] = 0
-        print("Init data successfully")
-
-    def generate_random_data(self, count):
+    def load_data(self):
+        print("Starting load_data method")
         data = []
-        for _ in range(count):
-            data.append({
-                "id": random.randint(1, 100000),
-                "domain_name": f"{uuid.uuid4()}.com",
-                "ip_address": ".".join(map(str, (random.randint(0, 255) for _ in range(4)))),
-                "file_hash": str(uuid.uuid4()),
-                "email": f"{uuid.uuid4()}@example.com"
-            })
+        i = 0
+        while True:
+            try:
+                chunk = list(self.collection.find({}, {"_id": 1, "ip_address": 1, "domain": 1, "sha256": 1}).skip(i).limit(self.chunk_size))
+                if not chunk:  # no more documents to fetch
+                    break
+                data.extend(self.convert_objectid(chunk))
+                print(f"Loaded chunk {i // self.chunk_size + 1}")
+                i += self.chunk_size
+            except Exception as e:
+                logging.error(f"Error loading data: {e}")
+                break
+        print("Finished load_data method")
         return data
 
-    def save_to_json(self, data):
-        with open('domain.json', 'w') as f:
-            json.dump([d["domain_name"] for d in data], f)
-        with open('ip.json', 'w') as f:
-            json.dump([d["ip_address"] for d in data], f)
-        with open('file.json', 'w') as f:
-            json.dump([d["file_hash"] for d in data], f)
-        with open('email.json', 'w') as f:
-            json.dump([d["email"] for d in data], f)
+    def init_data(self):
+        print("Starting init_data method")
+        index = 0
+        for d in self.data:
+            index = index + 1
+            print(f"Processing data: {index}")
+            self.mp[d['_id']] = d
+            self.link[0][d['_id']] = []
+            self.link[1][d['_id']] = []
+            self.link[2][d['_id']] = []
+            self.vis[d['_id']] = 0
 
-    def load_from_json(self):
-        with open('domain.json', 'r') as f:
-            domain = json.load(f)
-        with open('ip.json', 'r') as f:
-            ip = json.load(f)
-        with open('file.json', 'r') as f:
-            file = json.load(f)
-        with open('email.json', 'r') as f:
-            email = json.load(f)
+            self.link[0][d['_id']] = self.get_related_ids(d['ip_address'], self.ip_address_collection, 'ip_address', d['_id'])
+            self.link[1][d['_id']] = self.get_related_ids(d['domain'], self.domain_collection, 'domain', d['_id'])
+            self.link[2][d['_id']] = self.get_related_ids(d['sha256'], self.sha256_collection, 'sha256', d['_id'])
+        print("Finished init_data method")
 
-        return domain, ip, file, email
+    def get_related_ids(self, field_value, collection, field, _id):
+        if field_value is not None:
+            # Check if a document with the value field_value exists in the collection
+            doc = collection.find_one({"value": field_value})
+            if doc is None:
+                # If not, find all related _id's
+                related_docs = self.convert_objectid(list(self.collection.find({field: field_value}, {"_id": 1})))
+                # Create a document with the field_value as the value and the related _id's as the elements, and add it to the collection
+                collection.insert_one({"value": field_value, "elements": [doc['_id'] for doc in related_docs if doc['_id'] != _id]})
+            elif len(doc["elements"]) == 0:
+                # If there's a unique relation return empty array
+                return []
+            else:
+                # If a document with the value field_value exists, fetch the related _id's from the elements of the document
+                related_docs = [related_id for related_id in doc["elements"] if related_id != _id]
+            return related_docs
+        return []
 
-    def insert_to_mongodb(self, count):
-        domain, ip, file, email = self.load_from_json()
-
-        client = MongoClient(self.mongo_uri)
-        db = client[self.mongo_db]
-        collection = db[self.mongo_dummy_collection]
-
-        # Create an index on id for faster lookups and updates
-        collection.create_index("id")
-        collection.create_index("domain_name")
-        collection.create_index("ip_address")
-        collection.create_index("file_hash")
-
-        for _ in range(count // self.chunk_size):
-            data = []
-            for _ in range(self.chunk_size):
-                data.append({
-                    "id": random.randint(1, 100000),
-                    "domain_name": random.choice(domain),
-                    "ip_address": random.choice(ip),
-                    "file_hash": random.choice(file),
-                    "email": random.choice(email)
-                })
-
-            # Prepare operations for bulk_write
-            operations = [UpdateOne({"id": d["id"]}, {"$set": d}, upsert=True) for d in data]
-
-            # Perform bulk_write
-            collection.bulk_write(operations)
 
     def generate_relation(self, idx, state):
-        #if idx not in self.link[state]:  # Base case
-        #    return
         self.count = self.count + 1
-        if count % 10000 == 0:
-            print(f'{count} relations have been generated.')
+        if self.count % 10000 == 0:
+            print(f'{self.count} relations have been generated.')
         ret = copy.deepcopy(self.mp[idx])
 
         for i in range(0, 3):
@@ -126,11 +97,9 @@ class Relationship:
             array = []
 
             for des in self.link[i][idx]:
-                if self.vis[des['id']] == 0:
-                    self.vis[des['id']] = 1
-                    result = self.generate_relation(des['id'], i)
-                  #  self.vis[des['id']] = 0
-
+                if self.vis[des] == 0:  # Change this line
+                    self.vis[des] = 1   # And this line
+                    result = self.generate_relation(des, i)  # And this line
                     array.append(result)
 
             if len(array) > 0:
@@ -147,9 +116,9 @@ class Relationship:
         self.result = []
         print(self.data)
         for node in self.data:
-            self.vis[node['id']] = 1
-            self.result.append(self.generate_relation(node['id'], -1))
-           # self.vis[node['id']] = 0
+            self.vis[node['_id']] = 1
+            self.result.append(self.generate_relation(node['_id'], -1))
+           # self.vis[node['_id']] = 0
 
         with open('relations.json', 'w') as f:
             json.dump(self.result, f, indent=4)
